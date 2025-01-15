335d724c153e1720d709ab465a99e890fa9a9e37
Hritik Agrahari <hritik.a.agrahari@oracle.com>
2025-01-16 01:34:05 +0530

    Updates

---
+++ precache-manifest.3016be08f97a482a96739e293747609c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1aa890de82ed72192d525ad32e87f5",
+    "url": "/DevMeShowcase/index.html"
+  },
+  {
+    "revision": "ae8f3ca563f790b243f6",
+    "url": "/DevMeShowcase/static/css/3.2e053274.chunk.css"
+  },
+  {
+    "revision": "6eb0faad8d4f1ed93798",
+    "url": "/DevMeShowcase/static/css/4.36e89989.chunk.css"
+  },
+  {
+    "revision": "433f34676d5a6e5189c8",
+    "url": "/DevMeShowcase/static/css/main.e9fb12ab.chunk.css"
+  },
+  {
+    "revision": "f008154d1dff687379e0",
+    "url": "/DevMeShowcase/static/js/2.e9bd3f81.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/DevMeShowcase/static/js/2.e9bd3f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8f3ca563f790b243f6",
+    "url": "/DevMeShowcase/static/js/3.0feae902.chunk.js"
+  },
+  {
+    "revision": "6eb0faad8d4f1ed93798",
+    "url": "/DevMeShowcase/static/js/4.d0f8f631.chunk.js"
+  },
+  {
+    "revision": "433f34676d5a6e5189c8",
+    "url": "/DevMeShowcase/static/js/main.ae0fb6e8.chunk.js"
+  },
+  {
+    "revision": "111437debc996ab0e44d",
+    "url": "/DevMeShowcase/static/js/runtime-main.f74db8b3.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/DevMeShowcase/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/DevMeShowcase/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "715b16d2928728b01c45784705a239a9",
+    "url": "/DevMeShowcase/static/media/SAFe-logo.715b16d2.png"
+  },
+  {
+    "revision": "c1de69f89663992f86725e1dd9ae6bea",
+    "url": "/DevMeShowcase/static/media/androidLogo.c1de69f8.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/DevMeShowcase/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "20f8b1e8eeaf6d2a51dc7bacc75e2e5b",
+    "url": "/DevMeShowcase/static/media/covidTrackerLogo.20f8b1e8.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/DevMeShowcase/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/DevMeShowcase/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "2dc47d0dad4168b4ea81c356bea6854f",
+    "url": "/DevMeShowcase/static/media/nitSilcharLogo.2dc47d0d.png"
+  },
+  {
+    "revision": "e9069475b76406068fffbc8ab14ad41b",
+    "url": "/DevMeShowcase/static/media/ociLogo.e9069475.png"
+  },
+  {
+    "revision": "e7fdc9aa1a1091c324f9688cfd71e9e1",
+    "url": "/DevMeShowcase/static/media/oraclelogo.e7fdc9aa.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/DevMeShowcase/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "29df1b045f2e912fe0dc218d4525ce4b",
+    "url": "/DevMeShowcase/static/media/splitIsFunLogo1.29df1b04.png"
+  },
+  {
+    "revision": "5f4529d8f76ad590ec9007b9d5469ce2",
+    "url": "/DevMeShowcase/static/media/springLogo.5f4529d8.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/DevMeShowcase/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "76ad8e258ce817c6c0d7b51e23186c67",
+    "url": "/DevMeShowcase/static/media/upjaoLogo.76ad8e25.png"
+  },
+  {
+    "revision": "ce0e04044a335d4392354d61703993ea",
+    "url": "/DevMeShowcase/static/media/wikipediaSearchEngine.ce0e0404.png"
+  }
+]);
